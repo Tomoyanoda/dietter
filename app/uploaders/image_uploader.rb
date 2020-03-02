8b01281b55a7afad306d8be86f58582460911f52
tomoya.noda <sakashimayui@gmail.com>
2020-03-02 11:43:04 +0000

    fix carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale :resize_to_limit => [500, 500]
+  # process scale :resize_to_limit => [500, 500]
   #
   # def scale(width, height)
   #   # do something
