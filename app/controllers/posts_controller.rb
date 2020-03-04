146bb0b007cfdf3f3bf00aae68481a01b51667c9
tomoya.noda <sakashimayui@gmail.com>
2020-03-04 00:53:00 +0000

    timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
       flash[:success] = 'Message posted.'
       redirect_to root_url
     else
-      @posts = current_user.posts.order(id: :desc).page(params[:page])
+      @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Posting message failed.'
       render 'toppages/index'
     end
